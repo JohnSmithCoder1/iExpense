e67a264d9113de99f67a1958b88dc65acc4b65c3
John Smith <johnsmithcoder1@gmail.com>
2023-01-16 11:34:03 -0600

    Create ExpenseItem struct

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by J S on 1/16/23.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
