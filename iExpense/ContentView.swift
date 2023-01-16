a34289951c025438ed42668dc2ea4cc8e29b5034
John Smith <johnsmithcoder1@gmail.com>
2023-01-16 11:40:31 -0600

    Create NavigationView and implement adding test items with toolbar button

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    
     var body: some View {
-        Text("Hello iExpense")
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+        }
     }
 }
 
