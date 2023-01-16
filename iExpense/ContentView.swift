27cbfb7ae53c37e7f52e3a845e06bca291da0a39
John Smith <johnsmithcoder1@gmail.com>
2023-01-16 11:33:40 -0600

    Return ContentView to its original state

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,28 +7,9 @@
 
 import SwiftUI
 
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    
-    let name: String
-    
-    var body: some View {
-        Button("Dismiss") {
-            dismiss()
-        }
-    }
-}
-
 struct ContentView: View {
-    @State private var showingSheet = false
-    
     var body: some View {
-        Button("Show Sheet") {
-            showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "Billy")
-        }
+        Text("Hello iExpense")
     }
 }
 
