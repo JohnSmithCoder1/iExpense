570069648b0bc727405852398930da9deb824567
John Smith <johnsmithcoder1@gmail.com>
2023-01-16 11:18:39 -0600

    Add project files

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by J S on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
